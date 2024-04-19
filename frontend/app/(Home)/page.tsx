604b617a89a3d8bf4f2064a53c153f2a9a44a683
Victor <victor1668wu@gmail.com>
2024-04-18 23:58:00 -0300

    Configurações do projeto para os testes unitários

--- frontend/app/(Home)/page.tsx
+++ frontend/app/(Home)/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import CustomButton from "./components/customButton";
+import CustomButton from "../components/customButton";
 import { useState } from "react";
 
 export default function Home() {
@@ -14,8 +14,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
-      <h1 className=" text-9xl font-medium">{text}</h1>
-      <CustomButton onClick={handleChangeText} />
+      <h1 className=" text-9xl font-medium">Hello World!</h1>
     </main>
   );
 }
