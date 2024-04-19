e637e23734d474803255f32bc9a921dcddb43f4f
Luiz Fernando Lima Leite <l248405@dac.unicamp.br>
2024-04-19 00:59:43 -0300

    Cria testes para Home e customButton e adiciona testId paracustomButton

---
+++ frontend/app/tests/customButton.test.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom'
+import { fireEvent, render, screen } from '@testing-library/react'
+import CustomButton from '../components/customButton'
+
+const mockClick = jest.fn()
+
+describe('customButton test', () => {
+  it('should render a button', () => {
+    render(<CustomButton onClick={mockClick} />)
+
+    expect(
+      screen.getByTestId('custombutton')
+    ).toBeInTheDocument()
+  })
+
+  it('should run the function specified at onClick prop after a click', () => {
+    render(<CustomButton onClick={mockClick} />)
+
+    const btn = screen.getByTestId('custombutton')
+    fireEvent.click( btn )
+
+    expect(
+        mockClick
+    ).toHaveBeenCalledTimes(1)
+  })
+})
