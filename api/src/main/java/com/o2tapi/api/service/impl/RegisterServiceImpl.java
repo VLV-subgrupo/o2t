b2a471a5fb9c08ef4e16cacf5e69f351fe4440c1
Vítor Paziam Magalhães <vitorpaziam@gmail.com>
2024-06-07 18:14:24 -0300

    refact: 2 bloasters and 1 dispensable

--- api/src/main/java/com/o2tapi/api/service/impl/RegisterServiceImpl.java
+++ api/src/main/java/com/o2tapi/api/service/impl/RegisterServiceImpl.java
@@ -15,6 +15,7 @@ import com.o2tapi.api.repository.RegisterRepository;
 import com.o2tapi.api.repository.UserRepository;
 import com.o2tapi.api.service.RegisterService;
 import com.o2tapi.api.service.UserService;
+import com.o2tapi.api.service.ValidationService;
 
 import net.bytebuddy.utility.RandomString;
 
@@ -30,35 +31,22 @@ public class RegisterServiceImpl implements RegisterService {
     @Autowired
     UserService userService;
 
+    @Autowired
+    ValidationService validationService;
+
     @Override
     public ResponseEntity<Register> startRegister(RegisterDTO registerDTO) {
         
-        // verify if the password has at least 8 characters
-        if (registerDTO.getPassword() == null || registerDTO.getPassword().length() < 8) {
-            throw new InvalidFieldFormat("Password must have at least 8 characters");
-        }
-
-        // verify if the name, sport and email are not null
-        if (registerDTO.getName() == null) {
-            throw new InvalidFieldFormat("Name is required");
-        }
-
-        if (registerDTO.getSport() == null) {
-            throw new InvalidFieldFormat("Sport is required");
-        }
-
-        if (registerDTO.getEmail() == null) {
-            throw new InvalidFieldFormat("Email is required");
-        }
+        // Refatoração do code smells (Bloaters)
+        validationService.validateNotEmptyFields(new String[] {registerDTO.getEmail(), registerDTO.getName(), registerDTO.getSport()});
+        validationService.validatePasswordField(registerDTO.getPassword());
 
         User user = userRepository.findByEmail(registerDTO.getEmail());
 
-        // verify if the user already exists
         if (user != null) {
             throw new UserAlreadyExists("User email "+registerDTO.getEmail()+" already exists");
         }
 
-        // create a new register
         Register newRegister = new Register();
         newRegister.setEmail(registerDTO.getEmail());
         newRegister.setPassword(registerDTO.getPassword());
@@ -77,12 +65,10 @@ public class RegisterServiceImpl implements RegisterService {
 
         Optional<Register> actualRegister = registerRepository.findByEmail(register.getEmail());
 
-        // verify if the register exists
         if (actualRegister.isEmpty()) {
             throw new InvalidFieldFormat("Register not found");
         }
 
-        // verify if the hash is correct
         if (!actualRegister.get().getHash().contentEquals(register.getHash())) {
             throw new InvalidFieldFormat("Invalid hash");
         }
