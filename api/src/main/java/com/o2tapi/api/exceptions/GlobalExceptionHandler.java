c672877c9acc1033a3f8d77c5ad9de67204b7e22
Vítor Paziam Magalhães <vitorpaziam@gmail.com>
2024-06-20 16:34:58 -0300

    feat: Add exception handlers for EntityNotFound, InvalidFieldFormat, PasswordUnmatch, and UserAlreadyExists

--- api/src/main/java/com/o2tapi/api/exceptions/GlobalExceptionHandler.java
+++ api/src/main/java/com/o2tapi/api/exceptions/GlobalExceptionHandler.java
@@ -1,32 +1,35 @@
 package com.o2tapi.api.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * Global exception handler for the application.
- * This class provides a central place to handle all unexpected exceptions that occur within the application.
- * It captures exceptions and translates them into user-friendly messages, while also logging the details
- * for debugging purposes.
- */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    /**
-     * Handles generic exceptions thrown by any method in the application.
-     * This handler ensures that no exception goes uncaught, thereby preventing any unhandled exceptions
-     * from causing the application to crash.
-     *
-     * @param e The exception that was caught.
-     * @return A user-friendly message indicating an error has occurred, alongside the exception's own message.
-     * @throws Exception Indicates that this method itself might rethrow any unexpected exceptions.
-     */
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleException(Exception e) {
-        // Log the exception details here or perform other error handling
-        return "Error occurred: " + e.getMessage();
+    @ExceptionHandler(EntityNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFound ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidFieldFormat.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleInvalidFieldFormat(InvalidFieldFormat ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PasswordUnmatch.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<String> handlePasswordUnmatch(PasswordUnmatch ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(UserAlreadyExists.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExists ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 }
