f6dd7d6ff4e315fb29ee90579b2a71232693380c
Vítor Paziam Magalhães <vitorpaziam@gmail.com>
2024-06-20 16:34:42 -0300

    chore: Add validation methods for labels in ValidationService

--- api/src/main/java/com/o2tapi/api/service/impl/ValidationServiceImpl.java
+++ api/src/main/java/com/o2tapi/api/service/impl/ValidationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.o2tapi.api.service.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,11 @@ import org.springframework.stereotype.Service;
 
 import com.o2tapi.api.exceptions.EntityNotFound;
 import com.o2tapi.api.exceptions.InvalidFieldFormat;
+import com.o2tapi.api.models.Label;
 import com.o2tapi.api.models.User;
+import com.o2tapi.api.pojo.LabelDTO;
 import com.o2tapi.api.repository.UserRepository;
+import com.o2tapi.api.repository.LabelRepository;
 import com.o2tapi.api.service.ValidationService;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -19,13 +23,15 @@ public class ValidationServiceImpl implements ValidationService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private LabelRepository labelRepository;
 
     @Override
     public User validateUser(Long id) {
         Optional<User> user = userRepository.findById(id);
 
         if (!user.isPresent()) {
-            throw new EntityNotFoundException("User not found");
+            throw new EntityNotFoundException("User not found with id " + id);
         }
 
         return user.get();
@@ -43,7 +49,7 @@ public class ValidationServiceImpl implements ValidationService {
     public void validateNotEmptyFields(String[] fields) {
         for (String f : fields) {
             if (f == null || f.isEmpty()) {
-                throw new InvalidFieldFormat("The field "+f+" is required");
+                throw new InvalidFieldFormat("The field " + f + " is required");
             }
         }
     }
@@ -54,4 +60,53 @@ public class ValidationServiceImpl implements ValidationService {
         }
     }
 
+    @Override
+    public Label validateLabelId(Long id) {
+        Optional<Label> label = labelRepository.findById(id);
+
+        if (label.isEmpty()) {
+            throw new EntityNotFoundException("Label not found with id" + id);
+        }
+
+        return label.get();
+    }
+
+    @Override
+    public void validateLabelFields(LabelDTO labelDTO, boolean isUpdate) {
+        validateNotEmptyFields(new String[]{labelDTO.getName(), labelDTO.getColor()});
+
+        // Check if the user exists
+        User user = validateUser(labelDTO.getCreatedById());
+
+        // Check if the color format is valid
+        if (!isValidColorFormat(labelDTO.getColor())) {
+            throw new InvalidFieldFormat("Invalid color format. The color must be in the format #RRGGBB");
+        }
+        
+        // Check if the label name is unique for the user
+        if (!isLabelNameUnique(labelDTO.getName(), user, isUpdate)) {
+            throw new InvalidFieldFormat("Label name must be unique for the user");
+        }
+    }
+    
+    private boolean isValidColorFormat(String color) {
+        // Check if the color has the correct format (hexadecimal RGB)
+        return color.matches("^#([A-Fa-f0-9]{6})$");
+    }
+    
+    private boolean isLabelNameUnique(String name, User user, boolean isUpdate) {
+        // Check if the label name is unique for the user    
+        List<Label> labelsOfUser = labelRepository.findAllByCreatedBy(user);
+
+        if (isUpdate) { // Remove 'name' from labelsOfUser, since we are updating an existing label, so it can have the same name
+            labelsOfUser.removeIf(l -> l.getName().equals(name)); 
+        }
+
+        for (Label l : labelsOfUser) {
+            if (l.getName().equals(name)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
