29525e7f1f75fdc9ad5c763ebf8d3c1b65be1ff8
Vítor Paziam Magalhães <vitorpaziam@gmail.com>
2024-06-21 15:46:06 -0300

    refactor: Update WorkoutRepository to return a list of workouts by user and registration date

--- api/src/main/java/com/o2tapi/api/repository/WorkoutRepository.java
+++ api/src/main/java/com/o2tapi/api/repository/WorkoutRepository.java
@@ -2,7 +2,6 @@ package com.o2tapi.api.repository;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,5 +14,5 @@ public interface WorkoutRepository extends JpaRepository<Workout, Long> {
 
     List<Workout> findAllByCreatedBy(User user);
 
-    Optional<Workout> findByCreatedByAndRegistrationDate(User user, Date registrationDate);
+    List<Workout> findAllByCreatedByAndRegistrationDate(User user, Date registrationDate);
 }
