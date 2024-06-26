814b3ca9b409a369bbda18f574f19303687f5734
Luiz Fernando Lima Leite <l248405@dac.unicamp.br>
2024-06-26 02:35:07 -0300

    implementa handleRegister e handleGetUser

--- frontend/app/(Home)/conponents/profileForms.tsx
+++ frontend/app/(Home)/conponents/profileForms.tsx
@@ -1,11 +1,11 @@
 import CustomButton from "@/app/_components/customButton";
 import { DrawerHeader} from "@/app/_components/ui/drawer";
-
+import Cookies from "js-cookie";
 import { cn } from "@/app/_lib/utils";
 import Input from "./input";
-import { ChangeEvent, useEffect, useState } from "react";
-import { handleLogin } from "@/app/_lib/handlers";
-
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { handleLogin, handleRegister } from "@/app/_lib/handlers";
 
 type Prop = {
     className?: string,
@@ -13,9 +13,31 @@ type Prop = {
 }
 
 function SignForm({ className, signIn }: Prop) {
+    const router = useRouter()
+
+    const submitForm = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+
+        if (signIn) {
+            try {
+                handleLogin(formData)
+                router.push('/dashboard')
+            } catch (error) {
+                console.log("Error during login: ", error)
+            }
+        } else {
+            try {
+                handleRegister(formData)
+                router.push('/dashboard')
+            } catch (error) {
+                console.log("Error during register: ", error)
+            }   
+        }
+    }
 
     return (
-        <form onSubmit={handleLogin} className={cn("flex flex-col items-center gap-4 duration-[1000] transition-all ease-out", className)}>
+        <form onSubmit={submitForm} className={cn("flex flex-col items-center gap-4 duration-[1000] transition-all ease-out", className)}>
             <div className="grid gap-4 mb-4">
                 {!signIn && <Input id="username" name="username" />}
                 <Input id="email" name="email" type="email" />
--- frontend/app/_lib/handlers.ts
+++ frontend/app/_lib/handlers.ts
@@ -1,29 +1,55 @@
-import { FormEvent } from "react";
 import Cookies from "js-cookie";
 
-export const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
-    // Example API request using fetch
-    try {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
-        const response = await fetch('http://localhost:8080/v1/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: formData.get("email"),
-                password: formData.get("password"),
-            })
-        });
+export const handleLogin = async (formData: FormData) => {
+    const response = await fetch('http://localhost:8080/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: formData.get("email"),
+            password: formData.get("password"),
+        })
+    })
 
-        if (!response.ok) {
-            throw new Error('Login failed')
-        }
-
-        const data = await response.json()
-        Cookies.set('token', data.token, { expires: 7, secure: true });
-    } catch (error) {
-        console.error('Error during login:', error);
+    if (!response.ok) {
+        throw new Error('Login failed')
     }
-};
+    const data = await response.json()
+    Cookies.set('token', data.token, { expires: 7, secure: true });
+}
+
+export const handleRegister = async (formData: FormData) => {
+    const response = await fetch('http://localhost:8080/v1/auth/start', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: formData.get("username"),
+            email: formData.get("email"),
+            password: formData.get("password"),
+            sport: formData.get("sport")
+        })
+    })
+
+    if (!response.ok) {
+        throw new Error('Register failed')
+    }
+    handleLogin(formData)
+}
+
+export const handleGetUser = async (email: string) => {
+    const token = Cookies.get('token')
+    const response = await fetch('http://localhost:8080/v1/users/email' + email, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        },
+    })
+    if (!response.ok) {
+        throw new Error('Find User by Email failed')
+    }
+
+    return response.body
+}
